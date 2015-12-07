03a938346ec71a318726dabe519507571c949142
Osama Sidat <osama.sidat@d2l.com>
2015-12-07 10:32:44 -0500

    Added properties

--- TagSets/ViewController.swift
+++ TagSets/ViewController.swift
@@ -9,11 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var setNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // MARK: Actions
+    
 
 }
 
