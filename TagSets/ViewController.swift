2bd090c4eac5e3c06493f4e569a1543c8b817126
Osama Sidat <osama.sidat@d2l.com>
2015-12-11 14:35:30 -0500

    UITextfield delegates added

--- TagSets/ViewController.swift
+++ TagSets/ViewController.swift
@@ -8,19 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    // MARK: Properties
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var setNameLabel: UILabel!
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: Properties
+    @IBOutlet weak var tagsetLabelName: UILabel!
+    @IBOutlet weak var tagsetTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handle text field's user input through delegate callbacks
+        tagsetTextField.delegate = self
     }
     
-    // MARK: Actions
     
-
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        tagsetLabelName.text = textField.text
+    }
+    
+    
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        tagsetLabelName.text = "Default"
+    }
 }
 
