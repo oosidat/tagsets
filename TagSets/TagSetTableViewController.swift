4adff31b32f2922c222251ee3bfee70b654e14ca
Osama Sidat <osama.sidat@d2l.com>
2016-01-15 13:49:10 -0500

    Implemented navigation and connected the list and individual item view together

--- TagSets/TagSetTableViewController.swift
+++ TagSets/TagSetTableViewController.swift
@@ -20,12 +20,41 @@ class TagSetTableViewController: UITableViewController {
         loadSampleTagSets()
     }
     
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tagSets.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "TagSetTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! TagSetTableViewCell
+
+        let tagSet = tagSets[indexPath.row]
+        
+        cell.nameLabel.text = tagSet.name
+        
+        return cell
+    }
+    
     func loadSampleTagSets() {
         let set1 = TagSet(name: "Photography")!;
         let set2 = TagSet(name: "Cooking")!;
         let set3 = TagSet(name: "Travel")!;
         
         tagSets += [set1, set2, set3]
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func unwindToTagSetList(sender: UIStoryboardSegue) {
+        if let sourceViewController = sender.sourceViewController as? TagSetViewController, tagSet = sourceViewController.tagSet {
+            let newIndexPath = NSIndexPath(forRow: tagSets.count, inSection: 0)
+            tagSets.append(tagSet)
+            tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Bottom)
+        }
     }
 
 }
---
+++ TagSets/TagSetViewController.swift
@@ -0,0 +1,66 @@
+//
+//  TagSetViewController.swift
+//  TagSets
+//
+//  Created by Osama Sidat on 12/7/15.
+//  Copyright © 2015 Osama Sidat. All rights reserved.
+//
+
+import UIKit
+
+class TagSetViewController: UIViewController, UITextFieldDelegate {
+
+    // MARK: Properties
+
+    @IBOutlet weak var tagsetTextField: UITextField!
+    
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    var tagSet: TagSet?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Handle text field's user input through delegate callbacks
+        tagsetTextField.delegate = self
+        
+        checkValidTagSetName()
+    }
+    
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        saveButton.enabled = false
+    }
+    
+    func checkValidTagSetName() {
+        let text = tagsetTextField.text ?? ""
+        saveButton.enabled = !text.isEmpty
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        checkValidTagSetName()
+        navigationItem.title = textField.text
+    }
+    
+    // MARK: Navigation
+    
+    @IBAction func cancel(sender: UIBarButtonItem) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if saveButton === sender {
+            let name = tagsetTextField.text ?? ""
+            
+            tagSet = TagSet(name: name)
+            
+        }
+    }
+}
+
